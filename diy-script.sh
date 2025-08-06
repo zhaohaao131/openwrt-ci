2a440fd1f24e1cf2d163c5ff2ae8796dfab251a8
zhaohaao131 <657682618@qq.com>
2025-08-06 17:30:24 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -30,6 +30,7 @@ function git_sparse_clone() {
 }
 
 # 添加额外插件
+git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
 git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
